6c7c0dff9d6b3a526c3ba7e0e874509a8fb34ee9
Amr Saraya <amrmsaraya@gmail.com>
2022-04-21 17:49:19 +0200

    add DispatcherProvider to provide different dispatchers using hilt

---
+++ app/src/main/java/com/github/amrmsaraya/nagwa/di/DispatcherProvider.kt
@@ -0,0 +1,42 @@
+package com.github.amrmsaraya.nagwa.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DispatcherProvider {
+
+    @DefaultDispatcher
+    @Provides
+    @Singleton
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    @Singleton
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    @Singleton
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
